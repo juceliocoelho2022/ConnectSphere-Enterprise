86e4aca3311c13df56ca2a74c7e4d2960e08f7b4
Jucelio farias Coelho <juceliocoelho2022>
2026-08-01 09:32:06 -0300

    Initial project structure

---
+++ README.md
@@ -0,0 +1,27 @@
+# ConnectSphere Enterprise
+
+## Plataforma Social Corporativa
+
+Projeto desenvolvido utilizando arquitetura de microsserviços.
+
+## Tecnologias
+
+- Java 21
+- Spring Boot
+- PostgreSQL
+- Redis
+- Kafka
+- Docker
+- Kubernetes
+- React
+- Kotlin
+- Jetpack Compose
+
+## Estrutura
+
+- backend
+- frontend
+- android
+- infrastructure
+- docker
+- docs
---
+++ src/main/java/com/connectsphere/ConnectSphereEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.connectsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectSphereEnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectSphereEnterpriseApplication.class, args);
+    }
+
+}
